4895fa8cba97171158aa4cda61a9d110e9740cfe
GabrielePazi <pazigabriele@gmail.com>
2023-06-30 15:45:42 +0200

    1.1 inserimento in table con altri dati fissi

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 let distance = parseInt(prompt("Inserisci numero kilometri: "))
 let age = parseInt(prompt("Inserisci la tua età: "))
-let price = 0.21 * distance
 
-document.getElementById("ticketPrice").innerHTML = price.toFixed(2) + " €"
+let price = 0.21 * distance
 
 let sale = (20 * price) / 100
 
+document.getElementById("età").innerHTML = age + " anni"
+document.getElementById("distanza").innerHTML = distance + " Km"
+document.getElementById("prezzo").innerHTML = price.toFixed(2) + " €"
+
 if (age < 18) {
   document.getElementById("ticketPrice").innerHTML = (price - sale).toFixed(2) + " €"
+  document.getElementById("sconto").innerHTML = "- " + sale.toFixed(2) + " €"
 } else if (age > 65) {
   document.getElementById("ticketPrice").innerHTML = (price - (sale * 2)).toFixed(2) + " €"
+  document.getElementById("sconto").innerHTML = "- " + (sale * 2).toFixed(2) + " €"
+
+} else {
+  document.getElementById("ticketPrice").innerHTML = price.toFixed(2) + " €"
+  document.getElementById("sconto").innerHTML = "-0.00 €"
 }
